307690ba5bf321daa08a232c2e920b55864a24fa
Gustavo Cardoso Sousa <gustavo.sousa@sptech.school>
2024-10-03 11:47:23 -0300

    Iniciando processo de automatização de processos

---
+++ setup/setup-servidor.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# "Iniciando sript de automação" 
+
+sudo apt upgarde && sudo apt update -y
+
+echo 'Iniciando alterações de senha'
+echo 'Ubuntu:urubu100' | sudo chpasswd
+
+echo 'Root:urubu100' | sudo chpasswd
+
+sudo apt install git 
+
+echo '====================='
+echo 'Verificando versão do Git'
+git --version
+echo '====================='
+
+sudo apt install docker.io
+
+echo '====================='
+echo 'Verificando versão do Docker'
+docker --version
+echo '====================='
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+git clone https://github.com/EduardoAAzevedo/ServGuard.git
+
+cd /ServGuard/setup/
+
+sudo docker build -f ./dockerfile-node -t servguard-server-image .
+
+echo '====================='
+echo 'Verificando se a imagem foi criada'
+sudo docker images
+echo '====================='
+
+sudo docker run -d --name servguard-server -p 8080:8080 servguard-server-image
+
+sudo docker 
+
+
