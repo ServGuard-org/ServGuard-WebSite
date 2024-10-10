7965faab7862d185756e1ec732ef6d0e440d9705
Marcio-Juliao <marcio.juliao@sptech.school>
2024-10-10 19:30:22 -0300

    Atualizacao nas models

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function autenticar(email, senha) {
     var instrucaoSql = `
-        SELECT idUsuario, nome, email, fkEmpresa as empresaId, isAdm FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, nome, email, fkEmpresa as empresaId, isAdm FROM Usuario WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -13,26 +13,26 @@ function cadastrar(nome, email, senha, isAdm, cnpj, idEmpresa) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (idUsuario, fkEmpresa, nome, email, senha, isAdm) VALUES (DEFAULT, (SELECT idEmpresa FROM empresa WHERE ${condicao}), '${nome}', '${email}', '${senha}', ${isAdm});
+        INSERT INTO Usuario (idUsuario, fkEmpresa, nome, email, senha, isAdm) VALUES (DEFAULT, (SELECT idEmpresa FROM empresa WHERE ${condicao}), '${nome}', '${email}', '${senha}', ${isAdm});
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function alterar(nome, email, isAdm, idUsuario) {
-    let instrucaoSql = `UPDATE usuario SET nome = '${nome}', email = '${email}', isAdm = ${isAdm} WHERE idUsuario = ${idUsuario};`;
+    let instrucaoSql = `UPDATE Usuario SET nome = '${nome}', email = '${email}', isAdm = ${isAdm} WHERE idUsuario = ${idUsuario};`;
     console.log(`executando: ${instrucaoSql}`)
     return database.executar(instrucaoSql);
 }
 
 function ativar(idUsuario) {
-    let instrucaoSql = `UPDATE usuario SET isAtivo = 1 WHERE idUsuario = ${idUsuario};`;
+    let instrucaoSql = `UPDATE Usuario SET isAtivo = 1 WHERE idUsuario = ${idUsuario};`;
 
     return database.executar(instrucaoSql);
 }
 
 function inativar(idUsuario) {
-    let instrucaoSql = `UPDATE usuario SET isAtivo = 0 WHERE idUsuario = ${idUsuario};`;
+    let instrucaoSql = `UPDATE Usuario SET isAtivo = 0 WHERE idUsuario = ${idUsuario};`;
 
     return database.executar(instrucaoSql);
 }
@@ -53,7 +53,7 @@ function alterarSenha(nome, email, senha, idUsuario) {
 
     const mudaSenha = senha ? `, senha = '${senha}'` : ``;
 
-    const instrucaoSql = `UPDATE usuario SET nome = '${nome}', email = '${email}'${mudaSenha} WHERE idUsuario = ${idUsuario};`;
+    const instrucaoSql = `UPDATE Usuario SET nome = '${nome}', email = '${email}'${mudaSenha} WHERE idUsuario = ${idUsuario};`;
 
     console.log("Executando UPDATE: ", instrucaoSql);
 
