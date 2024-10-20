8057d2e4be615c5373e9380895af783b8c15b653
marcio <marcio.juliao@sptech.school>
2024-10-20 16:25:43 -0300

    Insert mock corrigidos

--- src/database/script-inserts-mock.sql
+++ src/database/script-inserts-mock.sql
@@ -25,10 +25,10 @@ INSERT INTO ServGuard.Volume (fkMaquina, pontoMontagem, capacidade) VALUES
 SET @volumeIdBase = LAST_INSERT_ID();
 
 INSERT INTO ServGuard.CapturaVolume (fkVolume, usado) VALUES
-    ((LAST_INSERT_ID() - 3), 100.000),   -- 100 GB
-    ((LAST_INSERT_ID() - 2), 200.000),   -- 200 GB
-    ((LAST_INSERT_ID() - 1), 400.000),   -- 400 GB
-    (LAST_INSERT_ID(), 1000.000);  -- 1 TB
+    ((LAST_INSERT_ID()), 100.000),   -- 100 GB
+    ((LAST_INSERT_ID() + 1), 200.000),   -- 200 GB
+    ((LAST_INSERT_ID() + 2), 400.000),   -- 400 GB
+    ((LAST_INSERT_ID() + 3), 1000.000);  -- 1 TB
 
 INSERT INTO ServGuard.MaquinaRecurso (fkMaquina, fkRecurso) VALUES
     (@maquinaId, (SELECT idRecurso FROM ServGuard.Recurso WHERE nome = 'usoCPU')),
