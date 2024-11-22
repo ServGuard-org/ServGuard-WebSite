b9c44f9f071588216782e99b5e44dfe782d087c4
Marcio-Juliao <marcio.juliao@sptech.school>
2024-11-21 23:52:35 -0300

    Atualização na lógica de busca da chave

--- src/models/externoModel.js
+++ src/models/externoModel.js
@@ -1,7 +1,7 @@
 var database = require("../database/config");
 
 function obterChaveGemini() {
-    const instrucaoSql = `SELECT chave FROM Chaves;`
+    const instrucaoSql = `SELECT chave FROM Chaves ORDER BY dthCriacao DESC LIMIT 1;`
 
     return database.executar(instrucaoSql);
 }
